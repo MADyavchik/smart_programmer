1b6def35e81f653dcf28fa84d9aad756e9cecef1
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-09-12 16:53:07 +0300

    buttest

--- Tests/buttest.py
+++ Tests/buttest.py
@@ -1,26 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
-# список пинов с кнопками
 buttons = [5, 6, 13, 19, 26]
 
 def button_callback(channel):
     print(f"Нажата кнопка на GPIO{channel}")
 
-# --- настройка GPIO ---
-GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.cleanup()          # сброс всех старых настроек!
+GPIO.setmode(GPIO.BCM)
 
 for pin in buttons:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # кнопки на GND
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_callback, bouncetime=200)
 
-print("Ожидание нажатий кнопок... Нажми Ctrl+C для выхода.")
-
+print("Ожидание кнопок... Нажми Ctrl+C для выхода.")
 try:
     while True:
-        time.sleep(0.1)
+        time.sleep(1)
 except KeyboardInterrupt:
-    print("\nВыход")
-finally:
     GPIO.cleanup()
+    print("Выход")
