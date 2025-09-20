c4b3c0379e96258c5b239f1f54489aab4a91c12b
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-09-21 01:49:44 +0300

    show_logo

--- show_logo.py
+++ show_logo.py
@@ -1,11 +1,17 @@
+import RPi.GPIO as GPIO
 from luma.core.interface.serial import spi
 from luma.lcd.device import st7789
 from PIL import Image
+
+# Включаем подсветку на GPIO12
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.OUT)
+GPIO.output(12, GPIO.HIGH)  # включить подсветку
 
 # Инициализация дисплея
 serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=16, bus_speed_hz=40000000)
 device = st7789(serial, width=320, height=240, rotate=0)
 
 # Загрузка и вывод логотипа
-logo = Image.open("logo.png").resize((320, 240)).convert("RGB")
+logo = Image.open("/root/smart_programmer/logo.png").resize((320, 240)).convert("RGB")
 device.display(logo)
