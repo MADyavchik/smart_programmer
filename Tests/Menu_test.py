60cbda3c64aab99202a033ae760fa0ad0c131f3d
Pavel_Uroshnikov <pavelurosnikov@MacBook-Air-Pavel-Uroshnikov.local>
2025-10-13 13:56:28 +0300

    log

--- Tests/Menu_test.py
+++ Tests/Menu_test.py
@@ -32,7 +32,7 @@ clock = pygame.time.Clock()
 font = pygame.font.Font(None, 20)
 
 # Text wrap
-def wrap_text(text, font, max_width, max_lines=2):
+def wrap_text(text, font, max_width, max_lines=3):
     """
     Делит текст на максимум max_lines, перенос по словам.
     Если текста больше, чем помещается, оставшаяся часть идёт в последнюю строку.
